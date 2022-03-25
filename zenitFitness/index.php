db739378e592c23d0c4141ad4bade6b18ba14d54
Matej Bendik <matejbendik.mb@gmail.com>
2022-03-25 18:15:50 +0100

    Adding fitness project

---
+++ zenitFitness/index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <title>Zenit Fitness</title>
+</head>
+
+<body>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mb-2 me-auto mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Úvod</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">O nás</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Ponúkame</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Galéria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Cenník</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Kontakt</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <button type="button" class="btn" id="registerBtn">Registrovať</button>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+    <div class="banner-image w-100 vh-100 d-flex justify-content-right align-items-center">
+        <div class="container">
+            <div class="col-lg-6 p-4" id="uvod">
+                <div style="clear: both">
+                    <h1 class="zenit">ZENIT&nbsp;</h1>
+                    <h1 class="fitness"> FITNESS </h1>
+                </div>
+                <p id="quote"> "Starajte sa o svoje telo. Je to jediné miesto, kde musíte žiť."
+                    (Jim Rohn)</p>
+                <div class="text-center">
+                    <button class="btn" id="loginBtn">Prihlásiť</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
